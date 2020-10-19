a396ae9b8163e44967e015428c365e0262193212
PS Subramanya Bhat <subramanya@robosoftin.com>
2020-10-20 00:22:15 +0530

    XOX Game

---
+++ xox-game/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import Game from './TicTacToe';
+import './App.css';
+
+function App() {
+  return (
+    <Game></Game>
+  );
+}
+
+export default App;
