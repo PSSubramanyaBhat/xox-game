8a5a8b2abb497109414f09fc0c063d8a4a101ab5
PS Subramanya Bhat <subramanya@robosoftin.com>
2020-10-24 03:46:22 +0530

    Added input fields, reset button, focus on i/p field, draw condition to the game

--- xox-game/src/SirTicTacToeGame.js
+++ xox-game/src/SirTicTacToeGame.js
@@ -4,11 +4,15 @@ import React, { useState } from 'react';
 
 import cn from 'classnames';
 
-const Square = ({ value, handleClick }) => {
+const Square = ({ value, handleClick, className }) => {
     return (
-        <button className="square" onClick={handleClick}>
+        <button id="resultBoard" className={cn('square', { squareWin: false })} onClick={handleClick}>
             {value}
         </button>
+
+        // <button className="square" onClick={handleClick}>
+        //     {value}
+        // </button>
     );
 };
 
@@ -40,6 +44,8 @@ const Board = ({ board, handleClick }) => {
 
 const Game = () => {
     let end = 0;
+    let focusClassName1 = "";
+    let focusClassName2 = "";
     const handleClick = (i) => {
         console.log(`square ${i} is clicked`);
         //We need to record this interaction in the board state
@@ -56,7 +62,8 @@ const Game = () => {
             const newBoard = [...board]; //Note, we have to create a new state object, and never mutate the current state and set it back. React wont come to know any state change in this case and there will be no re rendering that is going to happen
             newBoard[i] = player;
             //Flip the player
-            setPlayer(player === 'X' ? 'O' : 'X');
+            // setPlayer(player === 'X' ? 'O' : 'X');
+            setPlayer(player === player1 ? player2 : player1);
             //Set the board state
 
             const newHistory = history.concat([newBoard]);
@@ -64,15 +71,20 @@ const Game = () => {
             //Update the step
             setStep((prevStep) => prevStep + 1);
             setMoveCounter((x) => x + 1);
-            console.log(end);
-            console.log(moveCounter);
+            setCounter(tic => tic + 1);
         }
     };
 
     const [history, setHistory] = useState([Array(9).fill(null)]);
     const [step, setStep] = useState(0);
-    const [player, setPlayer] = useState('X');
+    // const [player, setPlayer] = useState('X');
     const [moveCounter, setMoveCounter] = useState(0);
+    const [count, setCounter] = useState(0);
+    const [player1, setIcon1] = useState('X');
+    const [player2, setIcon2] = useState('O');
+    // const [player1, setIcon1] = useState(document.getElementById("symbol1").value);
+    // const [player2, setIcon2] = useState(document.getElementById("symbol2").value);
+    const [player, setPlayer] = useState(player1);
 
     function computeWinner(board) {
         const lines = [
@@ -88,7 +100,53 @@ const Game = () => {
         for (let i = 0; i < lines.length; i++) {
             const [a, b, c] = lines[i];
             if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+                console.log("venbkbb");
+                // console.log(a);
+                // console.log(b);
+                // console.log(c);
+                console.log(board);
+                // console.log(board[a]);
+                console.log(lines[i]);
+                console.log(lines[i][0]);
+                console.log(lines[i][1]);
+                console.log(lines[i][2]);
                 return board[a];
+            }
+        }
+        return null;
+    }
+
+
+    function focusWinnerLine(board) {
+        const lines2 = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines2.length; i++) {
+            const [a, b, c] = lines2[i];
+            if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+
+
+                console.log(board);
+
+                console.log(lines2[i]);
+                console.log(lines2[i][0]);
+                console.log(lines2[i][1]);
+                console.log(lines2[i][2]);
+                return lines2[i];
+                // return (
+                //     <div className="board-row">
+                //         {renderSquare(6)}
+                //         {renderSquare(7)}
+                //         {renderSquare(8)}
+                //     </div>
+                // );
             }
         }
         return null;
@@ -99,7 +157,9 @@ const Game = () => {
         //and game should end.
         //We can actually derive if there is a winner. We dont need to maintain a seperate state
         //for this.
+
         const winner = computeWinner(history[step]);
+        /*
         if (winner === null) {
             return `Next player: ${player}`;
         } else if (moveCounter === 9 && end === 0) {
@@ -108,8 +168,16 @@ const Game = () => {
             end = 1;
             return `Player ${winner} won!`;
         }
+        */
 
-
+        if (winner) {
+            end = 1;
+            return `Player ${winner} won!`;
+        } else if (count === 9 && end === 0) {
+            return 'Its a DRAW game';
+        } else {
+            return `Next player: ${player}`;
+        }
     }
 
     const jumpToState = (step) => {
@@ -117,8 +185,6 @@ const Game = () => {
     };
 
     function renderHistory() {
-
-
         return history.map((b, index) => (
             // <li key={index}>{index === 0 ? 'Go to start of the game' : `Goto step${index}`}</li>
             <li key={index}>
@@ -136,13 +202,76 @@ const Game = () => {
     const board = history[step];
     return (
         <div className="game">
-            <div className="game-board">
-                <Board board={history[step]} handleClick={handleClick} />
+            <div class="PlayerInput">
+                <form>
+                    <label>P1:</label>
+                    <br></br>
+                    <input id="symbol1" placeholder="X"
+                        onChange={(event) => {
+                            setIcon1(event.target.value);
+                            setPlayer(player1);
+                        }}
+                    >
+
+                    </input>
+                </form>
+                <br></br>
+                <form>
+                    <label>P2:</label>
+                    <br></br>
+                    <input id="symbol2" placeholder="O"
+                        onChange={(event) => {
+                            setIcon2(event.target.value);
+                        }}
+                    >
+
+                    </input>
+                </form>
+                <button class="ResetButton"
+                    onClick={() => {
+                        end = 0;
+                        document.getElementById("symbol1").value = '';
+                        document.getElementById("symbol2").value = '';
+                        setIcon1('X');
+                        setPlayer(player1);
+                        setIcon2('O');
+                        setMoveCounter(0);
+                        setCounter(0);
+                        setStep(0);
+                        setHistory([Array(9).fill(null)]);
+                    }}
+                >
+                    Reset
+                </button>
+                <button class="PlayButton"
+                    onClick={() => {
+                        let len1 = document.getElementById("symbol1").value.length;
+                        let len2 = document.getElementById("symbol2").value.length;
+                        if (len1 === 1 && len2 === 1) {
+                            setIcon1(document.getElementById("symbol1").value); //Want to resolve thte issue of using this statement twice...
+                            setPlayer(player1);
+                        } else if (len1 === 0 && len2 === 0) {
+                            setIcon1('X'); //Want to resolve thte issue of using this statement twice...
+                            setIcon2('O');
+                            setPlayer(player1);
+                        } else {
+                            alert("Enter a single character");
+                            document.getElementById("symbol1").value = '';
+                            document.getElementById("symbol2").value = '';
+                        }
+
+                    }}
+                >
+                    Play
+                </button>
             </div>
-            <div className="game-info">
-                <div>{status()}</div>
-                <ol>{renderHistory()}</ol>
+            <div>
+                <div className="game-board">
+                    <Board board={history[step]} handleClick={handleClick} />
+                </div>
             </div>
+            <div class="Result">{status()}</div>
+            <ol class="History">{renderHistory()}</ol>
         </div>
     );
 };
